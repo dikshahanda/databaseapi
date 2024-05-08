40972e784d51c54a4933eb9d7f81b3a4b58730b8
Diksha <Dikshahanda2345@gmail.com>
2024-05-08 19:58:52 +0530

    app-port change

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 8000
+const port = 8000 || process.env.PORT
 const routeHandler = require('./routes')
 const router = routeHandler()
 const bodyParser = require('body-parser')
